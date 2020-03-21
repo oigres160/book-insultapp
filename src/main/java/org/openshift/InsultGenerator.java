3e0019e1b1534d7f4a97ffe22885badc82d3c625
Pisciottano, Sergio <sergio.pisciottano@accenture.com>
2020-03-21 11:59:29 +0100

    updated InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -34,7 +34,7 @@ public class InsultGenerator {
 			Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
 			if (connection != null) {
-
+				System.out.println("Performing query");
 				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
